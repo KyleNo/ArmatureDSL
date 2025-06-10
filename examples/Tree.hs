01f1ea216860d9ba7aa898d076dd2f9e5e20e972
KyleNo <kylenoble8@gmail.com>
2025-06-10 08:02:32 -0700

    added tree example

---
+++ examples/Tree.hs
@@ -0,0 +1,21 @@
+module Tree where
+
+import ArmatureDSL
+
+leaf = unit
+
+lj = joint (zrot (-20) @@ yrot 45)
+rj = joint (zrot 20 @@ yrot (-45))
+
+n1 = bone 1 +> [lj .-> leaf, rj .-> leaf]
+
+n2 = bone 2 +> [lj .-> n1, rj .-> n1]
+
+n3 = bone 4 +> [lj .-> n2, rj .-> n2]
+
+n4 = bone 8 +> [lj .-> n3, rj .-> n3]
+
+tree = arm (0, 0, 0) n4
+
+main :: IO ()
+main = writeArm tree "output\\tree.py"
